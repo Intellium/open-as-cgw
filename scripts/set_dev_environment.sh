53af0f7261f6d493951e67d48c2c65f3c05594e2
Dominic Schallert <ds@schallert.com>
2014-10-12 13:27:35 +0200

    Added a new sh script to set-up developer environment variables, Added missing README in ext/cyrus-sasl

---
+++ scripts/set_dev_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script is intended to set the needed environment variables
+# for an Open AS developer environment
+
+echo '+ Appending variables to your ~/.bashrc...'
+echo '' >> ~/.bashrc
+echo '# Open AS developer environment' >> ~/.bashrc
+echo 'export LIMESDEV=1' >> ~/.bashrc
+echo 'export LIMESLIB="$(dirname $(pwd))/lib/lib"' >> ~/.bashrc
+echo 'export LIMESGUI="$(dirname $(pwd))/gui/lib"' >> ~/.bashrc
+echo '+ Done! Please exit and re-login to your current shell!'

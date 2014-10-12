ad8b08d03095ca676dc5137bebf20dee94472a3f
Dominic Schallert <ds@schallert.com>
2014-10-12 13:36:18 +0200

    Fixed scripts/set_dev_environment.sh to use correct paths.

--- scripts/set_dev_environment.sh
+++ scripts/set_dev_environment.sh
@@ -6,6 +6,6 @@ echo '+ Appending variables to your ~/.bashrc...'
 echo '' >> ~/.bashrc
 echo '# Open AS developer environment' >> ~/.bashrc
 echo 'export LIMESDEV=1' >> ~/.bashrc
-echo 'export LIMESLIB="$(dirname $(pwd))/lib/lib"' >> ~/.bashrc
-echo 'export LIMESGUI="$(dirname $(pwd))/gui/lib"' >> ~/.bashrc
+echo 'export LIMESLIB="'$(dirname $(pwd))'/lib"' >> ~/.bashrc
+echo 'export LIMESGUI="'$(dirname $(pwd))'/gui/lib"' >> ~/.bashrc
 echo '+ Done! Please exit and re-login to your current shell!'

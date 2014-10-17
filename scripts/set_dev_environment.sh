7fb4f35d4cc58be1e8a0694a77102e26902e5fff
Dominic Schallert <ds@schallert.com>
2014-10-17 22:01:01 +0200

    Fixed set_dev_environment.sh to use correct base-path

--- scripts/set_dev_environment.sh
+++ scripts/set_dev_environment.sh
@@ -2,8 +2,19 @@
 # This script is intended to set the needed environment variables
 # for an Open AS developer environment
 
+# path vars
+SCRIPT=`readlink -f "$0"`
+SCRIPTPATH=`dirname "$SCRIPT"`
+cd $SCRIPTPATH
+
+# remove old vars to env 
+sed -i '/# Open AS developer environment/d' ~/.bashrc
+sed -i '/LIMESDEV/d' ~/.bashrc
+sed -i '/LIMESLIB/d' ~/.bashrc
+sed -i '/LIMESGUI/d' ~/.bashrc
+
+# append new vars to env
 echo '+ Appending variables to your ~/.bashrc...'
-echo '' >> ~/.bashrc
 echo '# Open AS developer environment' >> ~/.bashrc
 echo 'export LIMESDEV=1' >> ~/.bashrc
 echo 'export LIMESLIB="'$(dirname $(pwd))'/lib"' >> ~/.bashrc
